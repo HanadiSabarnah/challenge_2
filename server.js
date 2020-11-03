11d08070a626dd49264927fa68cd38f04abd6a1e
HanadiSabarnah <hsabarnah@gmail.com>
2020-11-03 15:28:32 +0200

    oh no

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", function (req, res) {
+    res.send("Hey, I am responding to your request!");
+   });
+
+
+
+app.listen(3000, function () {
+ console.log("App listening on port 3000!");
+});
 
